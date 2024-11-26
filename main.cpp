ef4d8d79e83848413df0575b71bd73f570a1f48e
Kevin Juselius <keffi_jv89@hotmail.com>
2024-11-26 19:24:09 +0100

    v1

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "lcd.h"
+
+// https://wokwi.com/projects/383283130065573889
+
+// PORTB B (digital pin 8 to 13)
+// C (analog input pins)
+// PORTD D (digital pins 0 to 7)
+#define LED_PIN 2
+#define BUTTON_PIN 1
+
+#define BIT_SET(a, b) ((a) |= (1ULL << (b)))
+#define BIT_CLEAR(a,b) ((a) &= ~(1ULL<<(b)))
+#define BIT_FLIP(a,b) ((a) ^= (1ULL<<(b)))
+#define BIT_CHECK(a,b) (!!((a) & (1ULL<<(b)))) 
+
+
+#define BUTTON_IS_CLICKED(PINB,BUTTON_PIN) !BIT_CHECK(PINB,BUTTON_PIN)
+
+int main(void){
+    HD44780 lcd;
+
+    lcd.Initialize(); // Initialize the LCD
+    lcd.Clear();      // Clear the LCD
+
+    lcd.WriteText((char *)"Hej hej");
+
+    // //Sätt till INPUT_PULLUP
+    // BIT_CLEAR(DDRB,BUTTON_PIN); // INPUT MODE
+    // BIT_SET(PORTB,BUTTON_PIN); 
+
+    // DATA DIRECTION = avgör mode
+    // om output så skickar vi  1 eller 0 på motsvarande pinne på PORT
+    // om input så läser vi  1 eller 0 på motsvarande pinne på PIN
+    //bool blinking = false;
+    while(1){
+     
+    }
+    return 0;
+}
