d3b685ecff126566905a07b23a175cae18fe9215
Jiredal <emmajiredal@hotmail.com>
2024-11-27 21:41:45 +0100

    en liten start hehehe

--- randFunk.cpp
+++ randFunk.cpp
@@ -1,1 +1,16 @@
+#include <random>
+
+bool randFunk(){
+
+
+    return true;
+
+// Loopar tills meddelandet inte slumpas samma för att säkerställa aldrig två gånger i rad.
+//return false;
+}
+
 // Hej. 
+RAND_MAX 
+RAND_MIN
+
+
