55e690b29b2ddaaaee7482d760f0ab04952d793a
Keffii <59940915+Keffii@users.noreply.github.com>
2024-11-28 08:57:27 +0100

    Update CustomerList.cpp

--- CustomerList.cpp
+++ CustomerList.cpp
@@ -27,18 +27,4 @@ int CustomerList::getTotalPayment(){
 int CustomerList::getCustomerCount(){
     return customerCount;
 }
-
-//I want a customerlist with information about message and paidamount
-//I want to be able to add a customer to the list
-int customerListTest(){
-    CustomerList cl;
-    cl.addCustomer(5000, "En god bilaffär (för Harry!)");
-    cl.addCustomer(3000, "Skynda innan Mårten ätit alla pajer");
-    cl.addCustomer(1500, "Bygga svart? Ring Petter");
-    cl.addCustomer(4000, "Mysterier? Ring Långben");
-    printf("Total payment: %d\n", cl.getTotalPayment());
-    printf("Customer count: %d\n", cl.getCustomerCount());
-    printf("Customer 0 message: %s\n", cl.getMessage(0));
-    printf("Customer 0 paid amount: %d\n", cl.getPaidAmount(0));
-    return 0;
 }
