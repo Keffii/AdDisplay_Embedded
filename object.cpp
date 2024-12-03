5a380e3546354660c2b2736597a8bcf6e4fcfcc2
victorGreat8 <Victor@erixon.one>
2024-12-03 11:28:19 +0100

    version 1

---
+++ object.cpp
@@ -0,0 +1,15 @@
+
+
+
+
+//Man får inte använda strings, vektor och inte iot string?
+
+//Skriv class för varje företag och sen object är deras egenskapar 
+//Så en class är ett företag, och jag gör en ny class för varje företag
+//och sedan gör jag en objekt för företages egenskaper, typ vad dom heter och ur dom har betalat?! Kolla vad som ska med i objektet.
+
+
+
+
+
+
