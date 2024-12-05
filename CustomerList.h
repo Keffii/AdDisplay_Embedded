a664afdeb1911a5bb2b207337befa7a86b84bd87
Keffii <59940915+Keffii@users.noreply.github.com>
2024-12-05 12:03:10 +0100

    Version 2

---
+++ CustomerList.h
@@ -0,0 +1,28 @@
+#ifndef __CUSTOMERLIST_H
+#define __CUSTOMERLIST_H
+
+
+struct Customer{            
+    int paidAmount;
+    char message[32];
+};
+
+class CustomerList{  
+  
+private: 
+    Customer customerList[10]; 
+    int customerCount = 0;
+    int totalPayment = 0;
+
+public: 
+    int addCustomer(int payment, char *m);
+    int getCustomerCount();
+
+    char* getMessage(int index);
+
+    int getPaidAmount(int index);
+        
+    int getTotalPayment();
+};
+
+#endif
